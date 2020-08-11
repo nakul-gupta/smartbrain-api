6303f8f3fcb57e83b0daff314b2c487a38fdbb41
nakul-gupta <nakulg333@gmail.com>
2020-08-10 19:30:21 -0700

    heroku config

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.use(cors())
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smartbrain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 })
 
